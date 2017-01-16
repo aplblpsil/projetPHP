d96f11efdef770e06d41a19441a39f8111675b41
steven <profil@10.0.2.15>
2017-01-16 17:42:40 +0100

    Mise en page: page de connexion (1.0) - Steven

--- vue/connexion_vue.php
+++ vue/connexion_vue.php
@@ -1,3 +1,14 @@
-<?php
-?>
-<h3>Page de connexion</h3>
+<div id="formLogin">
+    <form  method="POST" action="../modele/(...).php">
+        <fieldset>
+            <legend>Authentification</legend>
+            <!-- informations de connexion -->
+            <input type="text" id="ztLogin" name="ztLogin" placeholder="votre email..."><br />
+            <input type="password" id="ztPassword" name="ztPassword" placeholder="votre mot de passe..."><br /><br />
+            <!-- effacer/valider les champs -->
+            <input type="reset" value="effacer" class="btReset">
+            <input type="submit" value="valider" class="btSubmit">
+        </fieldset>
+    </form>
+    
+</div>
